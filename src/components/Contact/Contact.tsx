c0218c6dcfa15c8b53e74f4984d4b65fbc23b781
jsilf <jesilfver@gmail.com>
2023-03-04 13:36:39 +0100

    WIP styling and meta tags

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,11 +3,21 @@ import bgImage from "../../assets/ash-edmonds-0aWZdK8nK2I-unsplash.jpg";
 export const Contact = () => {
   return (
     <section className="p-standard" id="contact">
-      <div className="contact-bg">
-        <img src={bgImage} />
-      </div>
+      <figure className="contact-bg">
+        <img src={bgImage} alt="abstract background" />
+        <figcaption>
+          Photo by
+          <a href="https://unsplash.com/@badashphotos?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Ash Edmonds
+          </a>
+          on
+          <a href="https://unsplash.com/photos/0aWZdK8nK2I?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </figcaption>
+      </figure>
       <div className="contact-text display-flex display-flex-col">
-        <h2>Contact</h2>
+        <h2 className="p-2">Contact</h2>
 
         <div>
           <a href="https://github.com/jsilf" target="_blank">
