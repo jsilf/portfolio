ea31dea93cecbac3dcde60543ddb68e32950970d
jsilf <jesilfver@gmail.com>
2023-03-04 14:14:17 +0100

    WIP styling contact section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -66,27 +66,28 @@ export const About = () => {
       <section className="p-standard" id="about">
         <div className="display-flex align-center justify-center">
           <div className="text-block">
-            <h2 className="p-2">about</h2>
+            <h2 className="about-title">about</h2>
             <p>
-              I am currently studying to become a frontend developer by summer
-              2023. So far I have learned a lot about problem solving, how to
-              plan and design my own projects, work with git in groups, and
-              build websites with libraries and frameworks. Love learning new
-              things, but also work on getting better at what I already know.
+              I am studying to become a frontend developer by summer 2023. So
+              far I have learned a lot about problem solving, how to plan and
+              design my own projects, work with git in groups, and build
+              websites with libraries and frameworks. Love learning new things,
+              but also work on getting better at what I already know.
             </p>
             <p>
-              Tech I work with is HTML5, CSS3/SCSS, JavaScript, TypeScript,
-              Node.js. Frameworks and libraries such as React.js, Next.js,
-              Express.js, Angular. Databases MongoDB and MySQL.
+              The tech I currently work with consists of HTML5, CSS3/SCSS,
+              JavaScript, TypeScript, Node.js. Frameworks and libraries such as
+              React.js, Next.js, Express.js, Angular. Databases MongoDB and
+              MySQL.
             </p>
           </div>
-          <section className="text-block-list">
-            <h3 className="p-2">experience and education</h3>
+          {/* <section className="text-block-list">
+            <h3 className="about-subtitle">education and experience</h3>
             <div className="list-block display-flex">
               <Education education={educationList.label} />
               <Experience experience={experienceList.label} />
             </div>
-          </section>
+          </section> */}
         </div>
       </section>
     </>
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,30 +5,54 @@ export const Contact = () => {
     <section className="p-standard" id="contact">
       <figure className="contact-bg">
         <img src={bgImage} alt="abstract background" />
-        <figcaption>
-          Photo by
-          <a href="https://unsplash.com/@badashphotos?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        <figcaption className="contact-bg_caption">
+          Photo by{" "}
+          <a
+            href="https://unsplash.com/@badashphotos?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            target="_blank">
             Ash Edmonds
-          </a>
-          on
-          <a href="https://unsplash.com/photos/0aWZdK8nK2I?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          </a>{" "}
+          on{" "}
+          <a
+            href="https://unsplash.com/photos/0aWZdK8nK2I?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            target="_blank">
             Unsplash
           </a>
         </figcaption>
       </figure>
       <div className="contact-text display-flex display-flex-col">
-        <h2 className="p-2">Contact</h2>
+        <h2 className="contact-title">Contact</h2>
 
         <div>
-          <a href="https://github.com/jsilf" target="_blank">
-            <p>Check out my Github projects</p>
-          </a>
-          <a href="https://www.linkedin.com/in/jessica-silfver" target="_blank">
-            <p>Contact me on LinkedIn</p>
-          </a>
-          <a href="mailto:jesilfver@gmail.com" target="_blank">
-            <p>Email me</p>
-          </a>
+          <p>
+            Check out my{" "}
+            <a
+              href="https://github.com/jsilf"
+              target="_blank"
+              className="contact-link">
+              <span>Github projects</span>
+            </a>
+          </p>
+
+          <p>
+            Contact me on{" "}
+            <a
+              href="https://www.linkedin.com/in/jessica-silfver"
+              target="_blank"
+              className="contact-link">
+              <span>LinkedIn</span>
+            </a>
+          </p>
+
+          <p>
+            Or send me an{" "}
+            <a
+              href="mailto:jesilfver@gmail.com"
+              target="_blank"
+              className="contact-link">
+              <span>email</span>
+            </a>
+          </p>
         </div>
       </div>
     </section>
